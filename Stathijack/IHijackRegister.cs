f25e18a787e5d180a404489a34a345a7717e4c05
henridd <henri.oo@hotmail.com>
2024-05-06 16:43:37 +0200

    Add missing documentation for the HijackRegister.Register method

--- Stathijack/IHijackRegister.cs
+++ Stathijack/IHijackRegister.cs
@@ -11,7 +11,7 @@ namespace Stathijack
         /// <param name="target">The original class</param>
         /// <param name="hijacker">The fake class to redirect calls to</param>
         /// <param name="bindingFlags">Binding flags used to find the methods in the target class</param>
-        public void Register(Type target, Type hijacker);
+        void Register(Type target, Type hijacker);
 
         /// <summary>
         /// Register a class as a hijacker. It will scan the hijacker for methods with the same name and parameters as in the target
@@ -20,8 +20,11 @@ namespace Stathijack
         /// <param name="target">The original class</param>
         /// <param name="hijacker">The fake class to redirect calls to</param>
         /// <param name="bindingFlags">Binding flags used to find the methods in the target class</param>
-        public void Register(Type target, Type hijacker, BindingFlags bindingFlags);
+        void Register(Type target, Type hijacker, BindingFlags bindingFlags);
 
+        /// <summary>
+        /// Register a the provided mappings for hijacking.
+        /// </summary>
         void Register(IEnumerable<MethodReplacementMapping> mappings);
     }
 }
