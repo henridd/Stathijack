ec68fcce9e982115e19dfcd2c3f824748d84ec92
henridd <henri.oo@hotmail.com>
2024-05-06 16:56:41 +0200

    Transform MockingHijacker into a partial class

---
+++ Stathijack/Mocking/MockingHijacker.MockAll.cs
@@ -0,0 +1,67 @@
+﻿using System.Reflection;
+
+namespace Stathijack.Mocking
+{
+    public partial class MockingHijacker
+    {
+        /// <summary>
+        /// Hijacks every method that matches the methodName in the target type.
+        /// </summary>
+        /// <param name="methodName"></param>
+        /// <param name="action"></param>
+        public void MockAll(string methodName, Action action)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, action.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<T>(string methodName, Func<T> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<TIn, TOut>(string methodName, Func<TIn, TOut> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<T1, T2, TOut>(string methodName, Func<T1, T2, TOut> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<T1, T2, T3, TOut>(string methodName, Func<T1, T2, T3, TOut> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<T1, T2, T3, T4, TOut>(string methodName, Func<T1, T2, T3, T4, TOut> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+        public void MockAll<T1, T2, T3, T4, T5, TOut>(string methodName, Func<T1, T2, T3, T4, T5, TOut> function)
+        {
+            var mappings = CreateMappingForAllMethods(methodName, function.Method);
+            _hijackRegister.Register(mappings);
+        }
+
+		private List<MethodReplacementMapping> CreateMappingForAllMethods(string methodName, MethodInfo replacement)
+		{
+			var mappings = new List<MethodReplacementMapping>();
+			foreach (var method in _target.GetMethods())
+			{
+				if (method.Name == methodName)
+				{
+					mappings.Add(new MethodReplacementMapping(method, replacement));
+				}
+			}
+			return mappings;
+		}
+	}
+}
