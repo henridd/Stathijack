421d22f6eb09d087b1cac3483f0638a88740ba52
henridd <henri.oo@hotmail.com>
2024-06-16 10:36:55 +0200

    Merge MockingHijacker and MockingHijacker.MockMethod into a single class

--- Stathijack/Mocking/MockingHijacker.cs
+++ Stathijack/Mocking/MockingHijacker.cs
@@ -1,8 +1,9 @@
-﻿using System.Reflection;
+﻿using Stathijack.Exceptions;
+using System.Reflection;
 
 namespace Stathijack.Mocking
 {
-    public partial class MockingHijacker : IDisposable
+    public class MockingHijacker : IDisposable
     {
         private readonly Type _target;
         private readonly IHijackRegister _hijackRegister;
@@ -27,6 +28,50 @@ namespace Stathijack.Mocking
         public void Dispose()
         {
             _hijackRegister.Dispose();
+        }
+
+        /// <summary>
+        /// Hijacks the method that matches the methodName and the parameterTypes.
+        /// </summary>
+        /// <param name="methodName">The name of the method</param>
+        /// <param name="action">The action to replace the original method</param>
+        public HijackedMethodData MockMethod(string methodName, Action action)
+            => DoMockMethod(methodName, action.Method, action.Target);
+
+        public HijackedMethodData MockMethod<T>(string methodName, Func<T> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        public HijackedMethodData MockMethod<T, TOut>(string methodName, Func<T, TOut> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        public HijackedMethodData MockMethod<T1, T2, TOut>(string methodName, Func<T1, T2, TOut> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        public HijackedMethodData MockMethod<T1, T2, T3, TOut>(string methodName, Func<T1, T2, T3, TOut> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        public HijackedMethodData MockMethod<T1, T2, T3, T4, TOut>(string methodName, Func<T1, T2, T3, T4, TOut> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        public HijackedMethodData MockMethod<T1, T2, T3, T4, T5, TOut>(string methodName, Func<T1, T2, T3, T4, T5, TOut> function)
+            => DoMockMethod(methodName, function.Method, function.Target);
+
+        private HijackedMethodData DoMockMethod(string methodName, MethodInfo method, object? target)
+        {
+            if (string.IsNullOrWhiteSpace(methodName))
+                throw new ArgumentException("Method name cannot be null", nameof(methodName));
+
+            if (method == null)
+                throw new ArgumentNullException("You must provide a method to replace the original method", nameof(method));
+
+            var mappings = CreateMappingForMethods(methodName, method.GetParameters().Select(x => x.ParameterType).ToArray(), method);
+
+            if (mappings.Count > 1)
+            {
+                throw new MethodHijackingException("There were multiple methods found with the same name and parameters.");
+            }
+
+            return _hijackRegister.Register(mappings, target).First();
         }
 
         private List<MethodReplacementMapping> CreateMappingForMethods(string methodName, Type[]? parameterTypes, MethodInfo replacement)
